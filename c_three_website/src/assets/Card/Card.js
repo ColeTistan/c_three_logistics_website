5be4b2fea69cedad47f38bcadcb7d9575d0a118d
Cole Tistan <coletistan@gmail.com>
2021-03-22 12:05:17 -0400

    adjusted card styling

--- c_three_website/src/assets/Card/Card.js
+++ c_three_website/src/assets/Card/Card.js
@@ -7,11 +7,19 @@ const Card = () => {
       <div className="card mb-4">
         <div className="card-body">
           <div className="row">
-            <h6 className="card-subtitle mb-1 text-muted">Donald C</h6>
-            <i class="bi-alarm"></i>
+            <div className="inline">
+              <h6 className="card-subtitle text-muted text-center">Donald C</h6>
+              <div className="text-center">
+                <i className="fas fa-star text-warning"></i>
+                <i className="fas fa-star text-warning"></i>
+                <i className="fas fa-star text-warning"></i>
+                <i className="fas fa-star text-warning"></i>
+                <i className="fas fa-star text-warning"></i>
+              </div>
+            </div>
           </div>
-          <div className="card-text mb-3">A month ago</div>
-          <p className="card-text">"Great service"</p>
+          <div className="card-text text-center mb-3">A month ago</div>
+          <p className="card-text text-center">"Great service"</p>
         </div>  
       </div>   
     </>
