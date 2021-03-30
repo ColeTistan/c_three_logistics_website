94ad858ee5acce469ed75d2a9d53ad35222f4586
Cole Tistan <coletistan@gmail.com>
2021-03-29 21:21:55 -0400

    enabled mobile responsiveness to embedded map

--- c_three_website/src/components/Contact/Contact.js
+++ c_three_website/src/components/Contact/Contact.js
@@ -6,15 +6,17 @@ const Contact = () => {
       <div className="container">
       <h4 className="text-center mt-3 mb-3">Contact Us</h4>
         <div>
-          <iframe 
-            title="CThree Logistics HQ"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3076.7827456626883!2d-75.01497798463048!3d39.541954779476356!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c72cf8c7c56a1b%3A0x1d29662068194a43!2sC%20Three%20Logistics%20Llc!5e0!3m2!1sen!2sus!4v1616464434922!5m2!1sen!2sus" 
-            width="1100" 
-            height="500" 
-            allowFullScreen="" 
-            loading="lazy"
-          >
-          </iframe>
+          <div className="maps">
+            <iframe 
+              title="CThree Logistics HQ"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3076.7827456626883!2d-75.01497798463048!3d39.541954779476356!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c72cf8c7c56a1b%3A0x1d29662068194a43!2sC%20Three%20Logistics%20Llc!5e0!3m2!1sen!2sus!4v1616464434922!5m2!1sen!2sus" 
+              width="512" 
+              height="500" 
+              allowFullScreen="" 
+              loading="lazy"
+            >
+            </iframe>
+          </div>
         </div>
         <div className="row">
           <div className="col-sm">
--- c_three_website/src/components/Footer/Footer.js
+++ c_three_website/src/components/Footer/Footer.js
@@ -6,7 +6,8 @@ function Footer() {
       <footer className="bg-primary text-center text-lg-start">
         <div className="text-center p-3 text-white">
           &copy; 2021 Copyright: CThree Logistics LLC
-          <a className="text-white ml-5" href="https://cthreelogistics.com">
+          <br />
+          <a className="text-white" href="https://cthreelogistics.com">
             cthreelogistics.com
           </a>
         </div>
